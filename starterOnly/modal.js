00a86ab64259857458489ee51bd586eee84a7fc5
Killouh <killlouh@gmail.com>
2022-09-14 17:01:46 +0200

    first push

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,143 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// DOM Elements
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+
+// Main form 
+const formreserve = document.getElementById("reserve");
+
+// Found button by ID
+const closeBtn = document.getElementById("close");
+const btnSubmit = document.getElementById("btn-submit");
+
+// DOM Elements for FORM Check
+const inputFirst = document.getElementById("first");
+const inputLast = document.getElementById("last");
+const inputEmail = document.getElementById("email");
+const inputBirthdate = document.getElementById("birthdate");
+const checkBox1 = document.getElementById("checkbox1");
+const backDrop = document.querySelector(".backdrop")
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalBg.style.display = "none";
 }
 
 
+
+
+// Check if i can enable the button submit by checking if "checkbox" is checked
+function stateCheckBox() {
+  if (checkBox1.checked) {
+    btnSubmit.classList.remove("btn-hide");
+  } else {
+    btnSubmit.classList.add("btn-hide");
+  }
+}
+
+
+// Clear Input Formulaire
+function clearForm() {
+  const formInputs = document.querySelectorAll("form input");
+  formInputs.forEach(input => {
+    if (input.id !== "btn-submit") {
+      console.log(input.id);
+      document.getElementById(input.id).value = ""
+    }
+  }
+  );
+}
+
+// Minimal Age 15 ans 
+function minimalAge(age) {
+  const today = new Date();
+  let fullyear = today.getFullYear();
+  fullyear -= age;
+  inputBirthdate.setAttribute('max', fullyear + '-01-01');
+
+}
+
+// Set custom error message
+function customMessageInput(elementID, Message) {
+
+  if (elementID) {
+    elementID.addEventListener("input", () => {
+      elementID.setCustomValidity("");
+      elementID.checkValidity();
+    });
+
+    elementID.addEventListener("invalid", () => {
+      elementID.setCustomValidity(Message);
+    });
+  }
+}
+
+// Function validate form
+function validate() {
+  event.preventDefault(); // Avoid default behavior of form
+
+  // We check only State of checkbox since default behavior of HTML required and minlength do the job and prevent from sending form
+  if (checkBox1.checked) {
+    alert("Merci! Votre réservation a été reçue."); // Alert to show réservation is done
+    clearForm(); // Clear form input
+    closeModal(); // Close the form
+  } else {
+    alert(
+      "Vous devez accepter les conditions d'utilisation pour valider le formulaire !"
+    );
+  }
+}
+
+function main() {
+
+  // Detect if the form is on the page otherwise don't run the script 
+  if (formreserve) {
+    // launch modal event
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+    // Lauch close modal event
+    closeBtn.addEventListener("click", closeModal);
+    backDrop.addEventListener("click", closeModal);
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+         closeModal();
+      }
+    });
+    
+
+    // Lauch CheckBox Check each time we click on the Checkbox1 (T.O.S)
+    checkBox1.addEventListener("click", stateCheckBox);
+
+    // Set minimal age 
+    minimalAge(15);
+
+    // Custom message for input
+    customMessageInput(
+      inputFirst,
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    );
+    customMessageInput(
+      inputLast,
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    );
+    customMessageInput(
+      inputEmail,
+      "Veuillez entrer un email au bon format."
+    );
+    customMessageInput(
+      inputBirthdate,
+      "Vous devez entrer votre date de naissance."
+    );
+  }
+
+}
+
+main();
