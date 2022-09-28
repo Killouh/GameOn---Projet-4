80a8c624a82f213037058d13c4babb5b0bf1433a
Killouh <killlouh@gmail.com>
2022-09-28 15:48:13 +0200

    total reboot JS and HTML code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,143 +7,264 @@ function editNav() {
   }
 }
 
-
 // DOM Elements
 const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-
-// Main form 
-const formreserve = document.getElementById("reserve");
-
-// Found button by ID
-const closeBtn = document.getElementById("close");
-const btnSubmit = document.getElementById("btn-submit");
-
-// DOM Elements for FORM Check
-const inputFirst = document.getElementById("first");
-const inputLast = document.getElementById("last");
-const inputEmail = document.getElementById("email");
-const inputBirthdate = document.getElementById("birthdate");
+const modalBody = document.querySelector(".modal-body");
+const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit");
+const closeBtn = document.querySelector(".close");
+const thankYou = document.querySelector("#thank-you");
+const content = document.querySelector(".content");
+const body = document.querySelector("body");
+const everyFormInputs = document.querySelectorAll(".formData input");
+const formElement = document.querySelector(".modal-body form");
+const errorMessages = document.querySelectorAll(".error-msg");
+const backDrop = document.getElementById("backdrop")
 const checkBox1 = document.getElementById("checkbox1");
-const backDrop = document.querySelector(".backdrop")
+
+// regex
+const regexFirstLastName = /^[a-zA-Z\u00e0-\u00ff]+(([- ])?[a-zA-Z\u00e0-\u00ff])+$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexTournament = /\d$/;
+
+// date 
+let today = new Date();
+let date = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+
+
+
+console.log(date.getTime);
 
 // launch modal form
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 function launchModal() {
   modalBg.style.display = "block";
-}
+  body.style.overflow = "hidden";
 
-// close modal form
-function closeModal() {
-  modalBg.style.display = "none";
-}
-
-
-
-
-// Check if i can enable the button submit by checking if "checkbox" is checked
-function stateCheckBox() {
-  if (checkBox1.checked) {
-    btnSubmit.classList.remove("btn-hide");
-  } else {
-    btnSubmit.classList.add("btn-hide");
-  }
-}
-
-
-// Clear Input Formulaire
-function clearForm() {
-  const formInputs = document.querySelectorAll("form input");
-  formInputs.forEach(input => {
-    if (input.id !== "btn-submit") {
-      console.log(input.id);
-      document.getElementById(input.id).value = ""
+  errorMessages.forEach(errorMessage => {
+    if (errorMessage.style.display = "block") {
+      errorMessage.style.display = "none"
     }
+  })
+  
+
+}
+
+// close button 
+closeBtn.addEventListener("click", function() {
+  if(thankYou.style.display = "block") {
+    formElement.reset();
+    closeModal();
+    thankYou.style.display = "none";
+    submitBtn.value = "C'est parti!";
+    formData.forEach(form => {
+        form.style.display = "block"
+      });
+    
   }
-  );
-}
-
-// Minimal Age 15 ans 
-function minimalAge(age) {
-  const today = new Date();
-  let fullyear = today.getFullYear();
-  fullyear -= age;
-  inputBirthdate.setAttribute('max', fullyear + '-01-01');
-
-}
-
-// Set custom error message
-function customMessageInput(elementID, Message) {
-
-  if (elementID) {
-    elementID.addEventListener("input", () => {
-      elementID.setCustomValidity("");
-      elementID.checkValidity();
-    });
-
-    elementID.addEventListener("invalid", () => {
-      elementID.setCustomValidity(Message);
-    });
+  else {
+    closeModal();
   }
-}
+})
 
-// Function validate form
-function validate() {
-  event.preventDefault(); // Avoid default behavior of form
-
-  // We check only State of checkbox since default behavior of HTML required and minlength do the job and prevent from sending form
-  if (checkBox1.checked) {
-    alert("Merci! Votre réservation a été reçue."); // Alert to show réservation is done
-    clearForm(); // Clear form input
-    closeModal(); // Close the form
-  } else {
-    alert(
-      "Vous devez accepter les conditions d'utilisation pour valider le formulaire !"
-    );
-  }
-}
-
-function main() {
-
-  // Detect if the form is on the page otherwise don't run the script 
-  if (formreserve) {
-    // launch modal event
-    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-    // Lauch close modal event
-    closeBtn.addEventListener("click", closeModal);
-    backDrop.addEventListener("click", closeModal);
+// other Close Cases (Backdrop and Escape keybord btn)
+backDrop.addEventListener("click", closeModal);
     document.addEventListener("keydown", (event) => {
       if (event.key === "Escape") {
          closeModal();
       }
     });
-    
 
-    // Lauch CheckBox Check each time we click on the Checkbox1 (T.O.S)
-    checkBox1.addEventListener("click", stateCheckBox);
-
-    // Set minimal age 
-    minimalAge(15);
-
-    // Custom message for input
-    customMessageInput(
-      inputFirst,
-      "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
-    );
-    customMessageInput(
-      inputLast,
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
-    );
-    customMessageInput(
-      inputEmail,
-      "Veuillez entrer un email au bon format."
-    );
-    customMessageInput(
-      inputBirthdate,
-      "Vous devez entrer votre date de naissance."
-    );
-  }
-
+ // close modal function 
+function closeModal() {
+  modalBg.style.display = "none";
+  body.style.overflow = "auto";
 }
 
-main();
+// shows if errors were made by users
+
+//displays an error-message if `input` element is invalid
+
+function showValidation(input, isValid) {
+
+  // error trigger message
+  const errorElement = input.parentNode.querySelector(".error-msg")
+
+  if(isValid) {
+    errorElement.style.display = "none"; 
+    input.style.border = "none";
+  }
+  else {
+    errorElement.style.display = "block"; 
+    input.style.border = "4px solid red"; // => Est activé par défaut une fois le formulaire validé ,a voir
+  }
+  
+}
+
+// Clear trigger borders after validation 
+
+function clearErrors(){
+  errorElement.style.display = "none"; 
+  input.style.border = "none";
+}
+
+// inputs validation
+everyFormInputs.forEach(input => {
+  input.addEventListener("input", function() {
+    inputValidation(input)})
+})
+
+
+
+function inputValidation(input) {
+
+  // creation of a boolean to check if there is an error in at least one input
+
+  let isErrorInInput = false;
+
+  if(input.getAttribute("name") === "email") {
+    if(regexEmail.test(input.value)) {
+      showValidation(input, true)
+    }
+    else {
+      showValidation(input, false)
+      isErrorInInput = true;
+    }
+  }
+
+   else if(input.getAttribute("type") === "text") {
+      if(input.value.length >= 2 && regexFirstLastName.test(input.value)) {
+        showValidation(input, true)
+      }
+      else {
+        showValidation(input, false)
+        isErrorInInput = true;
+      }
+    }
+  
+    
+    else if(input.getAttribute("type") === "date") {
+      let dateSelected = new Date(document.querySelector(".formData #birthdate").value);
+
+      //Calcul  minimum age
+      function getAge(dateSelected) {
+        
+        let age = today.getFullYear() - dateSelected.getFullYear();
+        let minimumMonth = today.getMonth() - dateSelected.getMonth();
+        if (minimumMonth < 0 || (minimumMonth === 0 && today.getDate() < dateSelected.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+      
+      if(dateSelected.getTime() < date.getTime() && getAge(dateSelected)>= 15) {
+        showValidation( input, true);
+      }
+      else {
+        showValidation( input, false);
+        isErrorInInput = true;
+      }
+    }
+    
+    else if(input.getAttribute("type") === "number") {
+      if(regexTournament.test(input.value) && input.value >= 0 && input.value < 100) {
+        showValidation( input, true)
+      }
+      else {
+        showValidation( input, false)
+        isErrorInInput = true;
+      }
+    }
+    
+    else if(input.getAttribute("type") === "radio") {
+      const inputsRadio = document.querySelectorAll(".formData .radio-input");
+
+      // creation of a boolean to check if at least one of the  // 6 radio-input is checked
+     
+      let isAtLeastOneRadioChecked = false;
+
+      for (i in inputsRadio) {
+        if(inputsRadio[i].checked) {
+          isAtLeastOneRadioChecked = true;
+          break;
+        }
+      }
+
+      // isErrorInInput takes the opposite boolean value   // of isAtLeastOneRadioChecked
+    
+      isErrorInInput = !isAtLeastOneRadioChecked;
+      showValidation(input, isAtLeastOneRadioChecked);
+    }
+    
+    else if(input.getAttribute("type") === "checkbox" && input.hasAttribute("data-required")) {
+      if(input.checked) {
+        showValidation( input, true)
+      }
+      else {
+        showValidation( input, false)
+        isErrorInInput = true;
+      }
+    }
+  return isErrorInInput;
+}
+
+
+ //checks if there's an error in the form when submit btn is clicked and returns a boolean
+
+
+function checkForm() {
+  let isErrorInForm = false;
+
+  everyFormInputs.forEach(input => {
+    if(inputValidation(input)) {
+      isErrorInForm = true;
+      console.error("error on " , input)
+    }
+  })
+  return isErrorInForm
+}
+
+// Grey on button if CGU not checked
+
+checkBox1.addEventListener("click", stateCheckBox);
+function stateCheckBox() {
+  submitBtn.classList.toggle("btn-hide");
+  document.querySelector(".btn-submit").append("btn-hide");
+}
+
+
+
+formElement.addEventListener("submit", handleForm);
+
+
+ // checks if there's still an error in form and displays the "thank you" message if there's none
+
+
+function handleForm(e) {
+  e.preventDefault();
+  const isCheckFormErrors = checkForm();
+
+  if(isCheckFormErrors) {
+    console.log("button disabled")
+  }
+  else {
+    formElement.reset();
+    submitBtn.style.backgroundColor = "#FE142F";
+    formData.forEach(form => {
+      form.style.display = "none"
+    });
+    thankYou.style.display = "block";
+    submitBtn.value = "Fermer";
+    submitBtn.addEventListener("click", function() {
+      modalBg.style.display = "none";
+      thankYou.style.display = "none";
+      submitBtn.value = "C'est parti!";
+      formData.forEach(form => {
+        form.style.display = "block"
+      });
+    })
+  }
+}
